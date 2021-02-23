205b29b7ea29f5967f320e2a98af8cf05aca648f
Nguyen Khanh <khanhdsp@gmail.com>
2021-02-23 18:40:05 +0700

    Add new person class

---
+++ MyConsoleApp/MyConsoleApp/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Address { get; set; }
+    }
+}
